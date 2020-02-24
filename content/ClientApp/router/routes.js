db0e4bbb89f7a5e9c959bcb5001e5c65257ad936
Ty Moodley <ty.moodley@outlook.com>
2020-02-23 21:10:10 -0500

    updated router.

--- content/ClientApp/router/routes.js
+++ content/ClientApp/router/routes.js
@@ -1,7 +1,5 @@
-import HomePage from 'components/home-page'
 import Partner from 'components/partner'
 
 export const routes = [
-  { name: 'home', path: '/portal/dashboard', component: HomePage, display: 'Home', icon: 'home' },
-  { name: 'Partner', path: '/portal/Partner', component: Partner, display: 'Partner', icon: 'graduation-cap' }, 
+  { name: 'home', path: '/portal/dashboard', component: Partner, display: 'Home', icon: 'home' },
 ]
