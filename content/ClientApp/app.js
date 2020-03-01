d2a0cdd2638383e091a762b2b6a5bccc63dd6c00
Ty Moodley <ty.moodley@outlook.com>
2020-03-01 13:32:43 -0500

    Fixed Ui issues.

--- content/ClientApp/app.js
+++ content/ClientApp/app.js
@@ -5,13 +5,15 @@ import store from './store'
 import { sync } from 'vuex-router-sync'
 import App from 'components/app-root'
 import { FontAwesomeIcon } from './icons'
+import VueFormWizard from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueFormWizard from 'vue-form-wizard'
-import 'vue-form-wizard/dist/vue-form-wizard.min.css'
-Vue.use(VueFormWizard)
+
 Vue.use(BootstrapVue)
+Vue.use(VueFormWizard)
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon)
 Vue.prototype.$http = axios
---
+++ content/Controllers/PortalController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpingHands.Controllers
+{
+    [Authorize(Roles = "Partner, Administration")] 
+    public class PortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
