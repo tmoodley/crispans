d16f431a8a54a8d3cbbb86625b4975a0f89bc88d
Ty Moodley <ty.moodley@outlook.com>
2020-03-28 22:46:49 -0400

    Added Tabs to Partner Page.

--- content/ClientApp/components/partner.vue
+++ content/ClientApp/components/partner.vue
@@ -1,136 +1,114 @@
 <template>
   <div class="content">
     <div class="row">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <div class="card">
           <div class="card-header">
             <h5 class="title">Edit Profile</h5>
           </div>
           <div class="card-body">
-            <form v-on:submit="save">
-              <div class="row">
-                <div class="col-md-5 pr-1">
-                  <div class="form-group">
-                    <label>Company</label>
-                    <b-form-input v-model="store.company.companyName" placeholder="Enter Company Name"></b-form-input>
-                  </div>
-                </div>
-                <div class="col-md-3 px-1">
-                  <div class="form-group">
-                    <label>Username</label>
-                    <b-form-input v-model="store.company.username" placeholder="Username"></b-form-input>
-                  </div>
-                </div>
-                <div class="col-md-4 pl-1">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <b-form-input v-model="store.company.emailAddress" placeholder="Email"></b-form-input>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 pr-1">
-                  <div class="form-group">
-                    <label>First Name</label>
-                    <b-form-input v-model="store.company.givenName" placeholder="First Name"></b-form-input>
-                  </div>
-                </div>
-                <div class="col-md-6 pl-1">
-                  <div class="form-group">
-                    <label>Last Name</label>
-                    <b-form-input v-model="store.company.familyName" placeholder="Last Name"></b-form-input>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-12">
-                  <div class="form-group">
-                    <label>Address</label>
-                    <b-form-input v-model="store.company.address1" placeholder="Address"></b-form-input>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-4 pr-1">
-                  <div class="form-group">
-                    <label>City</label>
-                    <b-form-input v-model="store.company.city" placeholder="City"></b-form-input>
-                  </div>
-                </div>
-                <div class="col-md-4 px-1">
-                  <div class="form-group">
-                    <label>Province/State</label>
-                    <b-form-input v-model="store.company.state" placeholder="Province/State"></b-form-input>
-                  </div>
-                </div>
-                <div class="col-md-4 pl-1">
-                  <div class="form-group">
-                    <label>Postal Code/Zip</label>
-                    <b-form-input v-model="store.company.postalCode" placeholder="Postal/Zip"></b-form-input>
-                  </div>
-                </div>
-              </div>
-              <b-button variant="success" type="submit">SAVE</b-button>
-            </form>
+            <b-card>
+              <b-tabs pills card>
+                <b-tab body title="Details">
+                  <form v-on:submit="save">
+                    <div class="row">
+                      <div class="col-md-5 pr-1">
+                        <div class="form-group">
+                          <label>Company</label>
+                          <b-form-input v-model="store.company.companyName" placeholder="Enter Company Name"></b-form-input>
+                        </div>
+                      </div>
+                      <div class="col-md-3 px-1">
+                        <div class="form-group">
+                          <label>Username</label>
+                          <b-form-input v-model="store.company.username" placeholder="Username"></b-form-input>
+                        </div>
+                      </div>
+                      <div class="col-md-4 pl-1">
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">Email address</label>
+                          <b-form-input v-model="store.company.emailAddress" placeholder="Email"></b-form-input>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6 pr-1">
+                        <div class="form-group">
+                          <label>First Name</label>
+                          <b-form-input v-model="store.company.givenName" placeholder="First Name"></b-form-input>
+                        </div>
+                      </div>
+                      <div class="col-md-6 pl-1">
+                        <div class="form-group">
+                          <label>Last Name</label>
+                          <b-form-input v-model="store.company.familyName" placeholder="Last Name"></b-form-input>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label>Address</label>
+                          <b-form-input v-model="store.company.address1" placeholder="Address"></b-form-input>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-4 pr-1">
+                        <div class="form-group">
+                          <label>City</label>
+                          <b-form-input v-model="store.company.city" placeholder="City"></b-form-input>
+                        </div>
+                      </div>
+                      <div class="col-md-4 px-1">
+                        <div class="form-group">
+                          <label>Province/State</label>
+                          <b-form-input v-model="store.company.state" placeholder="Province/State"></b-form-input>
+                        </div>
+                      </div>
+                      <div class="col-md-4 pl-1">
+                        <div class="form-group">
+                          <label>Postal Code/Zip</label>
+                          <b-form-input v-model="store.company.postalCode" placeholder="Postal/Zip"></b-form-input>
+                        </div>
+                      </div>
+                    </div>
+                    <b-button variant="success" type="submit">SAVE</b-button>
+                  </form>
+                </b-tab>
+                <b-tab no-body title="Categories">
+                  <category></category>
+                </b-tab>
+                <b-tab no-body title="Certification">
+                  <certification></certification>
+                </b-tab>
+                <b-tab no-body title="Capability">
+                  <capability></capability>
+                </b-tab>
+                <b-tab no-body title="Company Type">
+                  <companytype></companytype>
+                </b-tab>
+                <b-tab title="File Type">
+                  <filetype></filetype>
+                </b-tab>
+                <b-tab title="Industry">
+                  <industry></industry>
+                </b-tab>
+                <b-tab title="Machine">
+                  <machine></machine>
+                </b-tab>
+                <b-tab title="Material">
+                  <material></material>
+                </b-tab>
+                <b-tab title="Naics">
+                  <naics></naics>
+                </b-tab>
+              </b-tabs>
+            </b-card> 
           </div>
         </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card card-user">
-          <div class="card-body">
-            <category></category>
-            <certification></certification>
-            <capability></capability>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <companytype></companytype>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <filetype></filetype>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <industry></industry>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <machine></machine>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <material></material>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <naics></naics>
-          </div>
-        </div>
-      </div>
-    </div>
+      </div> 
+    </div> 
   </div>
 </template>
 
@@ -185,5 +163,10 @@
 </script>
 
 <style>
-
+  li.nav-item a {
+    color: #9A9A9A;
+}
+    li.nav-item a:hover {
+    color: black;
+}
 </style>
