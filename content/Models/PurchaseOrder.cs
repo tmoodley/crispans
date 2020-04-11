02aa45c3fcd794fad8729bed5b42f20a1746ef97
Mamba Sun <bryant.kai1227@gmail.com>
2020-04-10 23:31:34 -0400

    formating purchase order form page

--- content/Models/PurchaseOrder.cs
+++ content/Models/PurchaseOrder.cs
@@ -21,5 +21,11 @@ namespace HelpingHands.Models
         public double Tax { get; set; }
         public double Total { get; set; }
         public List<PurchaseOrderItem> PurchaseOrderItems { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
     }
 }
