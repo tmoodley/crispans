57f4e534c3e48382825427b80dcb07d209cea8fe
Ty Moodley <ty.moodley@outlook.com>
2020-04-13 20:59:10 -0400

    Added updates for the menu button and list view.

--- content/PurchaseOrderApp/router/routes.js
+++ content/PurchaseOrderApp/router/routes.js
@@ -1,7 +1,9 @@
 const CreatePurchaseOrder = () => import(/* webpackChunkName: "home" */ 'components/purchaseorders/create.vue')
 const ManagePurchaseOrder = () => import(/* webpackChunkName: "home" */ 'components/purchaseorders/manage.vue')
+const PurchaseOrders = () => import(/* webpackChunkName: "home" */ 'components/purchaseorders/orders.vue')
 
 export const routes = [
   { name: 'CreatePO', path: '/PurchaseOrders/Create', component: CreatePurchaseOrder, display: 'Create Purchase Order', icon: 'home' },
-  { name: 'ManagePO', path: '/PurchaseOrders/Manage/:id', component: ManagePurchaseOrder, display: 'Manage Purchase Order', icon: 'home' }
+  { name: 'ManagePO', path: '/PurchaseOrders/Manage/:id', component: ManagePurchaseOrder, display: 'Manage Purchase Order', icon: 'home' },
+  { name: 'Orders', path: '/PurchaseOrders/Manage', component: PurchaseOrders, display: 'Purchase Orders', icon: 'home' }
 ]
