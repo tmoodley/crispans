cf0b07da71a9bf87137d94a82cf21f4bc211d5ce
Ty Moodley <ty.moodley@outlook.com>
2020-04-15 09:04:04 -0400

    added updates to the tenders process.

---
+++ content/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vue2Spa.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- content/PurchaseOrderApp/router/routes.js
+++ content/PurchaseOrderApp/router/routes.js
@@ -1,8 +1,12 @@
 const CreatePurchaseOrder = () => import(/* webpackChunkName: "home" */ 'components/purchaseorders/create.vue')
 const ManagePurchaseOrder = () => import(/* webpackChunkName: "home" */ 'components/purchaseorders/manage.vue')
- 
+const Job = () => import(/* webpackChunkName: "about" */ 'components/jobs/job.vue')
+const Jobs = () => import(/* webpackChunkName: "contact" */ 'components/jobs/jobs.vue')
 export const routes = [
   { name: 'CreatePO', path: '/PurchaseOrders/Create', component: CreatePurchaseOrder, display: 'Create Purchase Order', icon: 'home' },
   { name: 'ManagePO', path: '/PurchaseOrders/Manage/:id', component: ManagePurchaseOrder, display: 'Manage Purchase Order', icon: 'home' },
   { name: 'Buyer', path: '/Buyer/Create', component: CreatePurchaseOrder, display: 'Create Buyer Purchase Order', icon: 'home' },
+  { name: 'CreateTender', path: '/Tenders/Create', component: Job, display: 'Create Product', icon: 'info' },
+  { name: 'ManageTenders', path: '/Tenders/Manage:id', component: Jobs, display: 'Manage Products', icon: 'list' },
+  { name: 'SearchTenders', path: '/Tenders/Manage', component: Jobs, display: 'Search Products', icon: 'list' }
 ]
