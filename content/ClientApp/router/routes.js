3b9d236c171f6528eaabf74e74e1346caef653f4
Kai Sun <bryant.kai1227@gmail.com>
2020-04-25 22:47:15 -0400

    Change the manage tenders and manage purchase orders to tabs

--- content/ClientApp/router/routes.js
+++ content/ClientApp/router/routes.js
@@ -2,6 +2,7 @@ const Dashboard = () => import(/* webpackChunkName: "Partner" */ 'components/das
 const Partner = () => import(/* webpackChunkName: "Partner" */ 'components/partner.vue')
 const Job = () => import(/* webpackChunkName: "Job" */ 'components/jobs/job.vue')
 const Jobs = () => import(/* webpackChunkName: "Jobs" */ 'components/jobs/jobs.vue')
+const TenderEdit = () => import(/* webpackChunkName: "Job" */ 'components/tenders/edit.vue')
 const CreatePurchaseOrder = () => import(/* webpackChunkName: "CreatePurchaseOrder" */ 'components/purchaseorders/create.vue')
 const ManagePurchaseOrder = () => import(/* webpackChunkName: "ManagePurchaseOrder" */ 'components/purchaseorders/manage.vue')
 const PurchaseOrders = () => import(/* webpackChunkName: "PurchaseOrders" */ 'components/purchaseorders/orders.vue')
@@ -13,6 +14,7 @@ export const routes = [
   { name: 'createtender', path: '/portal/tender/create', component: Job, display: 'Create Tender', icon: 'info', showOnMenu: true },
   { name: 'searchtender', path: '/portal/tender/search', component: Jobs, display: 'Search Tenders', icon: 'list', showOnMenu: true },
   { name: 'managetenders', path: '/portal/tender/manage', component: Jobs, display: 'Manage Tenders', icon: 'list', showOnMenu: true },
+  { name: 'managetender', path: '/portal/tender/manage/:id', component: TenderEdit, display: 'Edit Tenders', icon: 'list', showOnMenu: false },
   { name: 'CreatePO', path: '/portal/PurchaseOrders/Create', component: CreatePurchaseOrder, display: 'Create Purchase Order', icon: 'home', showOnMenu: true },
   { name: 'ManagePO', path: '/portal/PurchaseOrders/Manage/:id', component: ManagePurchaseOrder, display: 'Manage Purchase Order', icon: 'home', showOnMenu: false },
   { name: 'Orders', path: '/portal/PurchaseOrders/Manage', component: PurchaseOrders, display: 'Purchase Orders', icon: 'home', showOnMenu: true },
