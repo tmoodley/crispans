adf5a2ad05404d017345f08bb3a6565744b5c699
Kai Sun <bryant.kai1227@gmail.com>
2020-05-08 16:08:06 -0400

    fix validation of tenders, purchase order and products. Make it has better user experience

--- content/ClientApp/components/purchaseorders/create.vue
+++ content/ClientApp/components/purchaseorders/create.vue
@@ -21,20 +21,22 @@
                   <div class="col-md-6">
                     <div class="form-group">
                       <label for="PurchaseDate" class="control-label">Purchase Date</label>
-                      <input v-model="purchaseOrder.PurchaseDate" type="date" class="form-control" />
+                      <input v-model="purchaseOrder.PurchaseDate" type="date" class="form-control" @blur="$v.purchaseOrder.PurchaseDate.$touch()" />
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.PurchaseDate.required"> This field must not be empty</p>
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
                       <label for="DeliveryDate" class="control-label">Delivery Date</label>
-                      <input v-model="purchaseOrder.DeliveryDate" type="date" class="form-control" />
+                      <input v-model="purchaseOrder.DeliveryDate" type="date" class="form-control" @blur="$v.purchaseOrder.DeliveryDate.$touch()" />
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.DeliveryDate.required"> This field must not be empty</p>
                     </div>
                   </div>
                   <div class="col-md-6" :class="{invalid: $v.purchaseOrder.PurchaseOrderNumber.$error}">
                     <div class="form-group">
                       <label for="PurchaseOrderNumber" class="control-label">Purchase Order Number</label>
                       <input v-model="purchaseOrder.PurchaseOrderNumber" class="form-control" @blur="$v.purchaseOrder.PurchaseOrderNumber.$touch()" />
-                      <p v-if="!$v.purchaseOrder.PurchaseOrderNumber.required && $v.purchaseOrder.PurchaseOrderNumber.$error"> This field must not be empty</p>
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.PurchaseOrderNumber.required"> This field must not be empty</p>
                     </div>
                   </div>
                 </div>
@@ -50,7 +52,7 @@
                   <div class="col-md-3">
                     <div class="form-group" :class="{invalid: $v.purchaseOrder.PurchaseOrderItems.$each[index].item.$error}">
                       <input type="text" class="form-control" placeholder="Item" v-model="item.item" @blur="$v.purchaseOrder.PurchaseOrderItems.$each[index].item.$touch()" />
-                      <p v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].item.required && $v.purchaseOrder.PurchaseOrderItems.$each[index].item.$error"> This field must not be empty</p>
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].item.required"> This field must not be empty</p>
                     </div>
                   </div>
                   <div class="col-md-4">
@@ -62,13 +64,13 @@
                     <div class="form-group" :class="{invalid: $v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.$error}">
                       <input type="number" class="form-control input_height" placeholder="Quantity" v-model="item.quantity" @blur="$v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.$touch()" />
                       <p v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.numeric"> Please provide a valid Quantity</p>
-                      <p v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.required && $v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.$error"> This field must not be empty</p>
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].quantity.required"> This field must not be empty</p>
                     </div>
                   </div>
                   <div class="col-md-2">
                     <div class="form-group" :class="{invalid: $v.purchaseOrder.PurchaseOrderItems.$each[index].amount.$error}">
                       <input type="text" class="form-control" placeholder="Price" v-model="item.amount" @change="debouncedGetAnswer()" @blur="$v.purchaseOrder.PurchaseOrderItems.$each[index].amount.$touch()" />
-                      <p v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].amount.required && $v.purchaseOrder.PurchaseOrderItems.$each[index].amount.$error"> This field must not be empty</p>
+                      <p :class="{ invalided: validate }" style="color: red;" v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].amount.required"> This field must not be empty</p>
                       <p v-if="!$v.purchaseOrder.PurchaseOrderItems.$each[index].amount.decimal"> Please provide a valid Amount</p>
                     </div>
                   </div>
@@ -101,7 +103,7 @@
                       </div>
                     </div>
                     <div class="form-group">
-                      <input type="submit" value="Create" class="btn btn-primary" :disabled="$v.$invalid" />
+                      <button type="submit" class="btn btn-primary">Create</button>
                     </div>
                   </div>
                 </div>
@@ -120,29 +122,30 @@
   import { required, numeric, minLength, maxLength, decimal } from 'vuelidate/lib/validators'
   export default { 
       data() {
-          return {
-              email: _user,
-              loading: false,
-              purchaseOrder: {
-                PurchaseDate: '',
-                DeliveryDate: '',
-                Notes: '',
-                PurchaseOrderNumber: '',
-                CustomerId: '',
-                Email: _user,
-                Status: '',
-                SubTotal: 0,
-                Tax: 0,
-                Total: 0,
-                PurchaseOrderItems: [{
-                  Item: '',
-                  Description: '',
-                  Quantity: 0,
-                  Amount: 0.00
-                }],
-              }
-            }
-          },
+      return {
+          validate: true,
+          email: _user,
+          loading: false,
+          purchaseOrder: {
+            PurchaseDate: '',
+            DeliveryDate: '',
+            Notes: '',
+            PurchaseOrderNumber: '',
+            CustomerId: '',
+            Email: _user,
+            Status: '',
+            SubTotal: 0,
+            Tax: 0,
+            Total: 0,
+            PurchaseOrderItems: [{
+              Item: '',
+              Description: '',
+              Quantity: 0,
+              Amount: 0.00
+            }],
+          }
+        }
+      },
       methods: {
           ...mapActions('company', [
             'getCompany'
@@ -155,17 +158,23 @@
             .post('/api/purchaseorders/', self.purchaseOrder)
             .then(response => { self.purchaseOrder = response.data }) 
           },
-          onComplete: function () {
-                var self = this;
-                this.save().then(function () { 
-                  self.$swal.fire(
-                    'Saved',
-                    'Job Saved',
-                    'success'
-                  ).then(function () { 
-                    self.$router.push({ path: '/purchaseorders/manage/' + self.purchaseOrder.id })
-                  })
-                });
+        onComplete: function () {
+           event.preventDefault(); // prevent submit form refresh the page
+            var self = this;
+            if (this.$v.$invalid == true) {
+              this.validate = false;
+              self.$swal.fire('Please make sure the required field is filled properly');
+            } else {
+              this.save().then(function () {
+                self.$swal.fire(
+                  'Saved',
+                  'Job Saved',
+                  'success'
+                ).then(function () {
+                  self.$router.push({ path: '/purchaseorders/manage/' + self.purchaseOrder.id })
+                })
+              });
+            }           
           }, 
           addLine() {
               this.purchaseOrder.PurchaseOrderItems.push({
@@ -174,7 +183,6 @@
                   Quantity: 0,
                   Amount: 0.00
               });
-
           },
           addTotal() {
               var _this = this;
@@ -194,6 +202,8 @@
           PurchaseOrderNumber: {
             required
           },
+          PurchaseDate: { required },
+          DeliveryDate: { required },
           PurchaseOrderItems: {
             required,
             $each: {
@@ -239,5 +249,8 @@
   .form-group.invalid input, .col-md-6.invalid input {
     border: 1px solid red;
     background-color: #ffc9aa;
+  }
+  .form-group p.invalided{
+      display: none;
   }
 </style>
